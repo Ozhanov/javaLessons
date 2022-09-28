124d0690828190a7f24e8e8fc1c4eb2dea4cbddb
Agyn Bolatov <agyn@brainchanger.io>
2022-09-28 13:04:14 +0600

    выполнил первое ДЗ2

--- src/geekbrains/lesson1/Homework1.java
+++ src/geekbrains/lesson1/Homework1.java
@@ -3,9 +3,14 @@ package geekbrains.lesson1;
 public class Homework1 {
     public static void main(String[] args) {
         task1();
+        //jbnhkjbghjkvjgv
     }
 
     private static void task1(){
+        System.out.println("Решение первой задачи");
+    }
+
+    private static void task2(){
         System.out.println("Решение первой задачи");
     }
 
